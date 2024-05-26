c4dc4372731cf9dbf7ae2828c18d581c463c221a
Juan Marini <160893745+juan-marini@users.noreply.github.com>
2024-05-26 20:27:43 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        type: 'bullets',
+    },
+});
